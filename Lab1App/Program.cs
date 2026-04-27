171678e5a4960502d6981bf97cd8dc88c404a172
Daniil <goltdaniil@gmail.com>
2026-04-27 20:27:50 +0700

    Client receve message

--- Lab1App/Program.cs
+++ Lab1App/Program.cs
@@ -1,9 +1,6 @@
-using System.Net;
 using System.Net.Sockets;
-using ProductTcpServer;
-using System.Reflection.Metadata.Ecma335;
+using System.Net;
 using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 
@@ -21,7 +18,7 @@ namespace LabApp
 
             var stream = client.GetStream();
 
-            ThreadStart startListening = () => Listening(stream);
+            ThreadStart startListening = () => Listener(stream);
 
             Thread listening = new Thread(startListening);
 
@@ -31,6 +28,12 @@ namespace LabApp
             {
                 string? messsage = Console.ReadLine();
 
+                if (messsage == "0")
+                {
+                    Console.WriteLine("Сеанс завершен");
+                    break;
+                }
+
                 byte[] data = Encoding.UTF8.GetBytes(messsage);
 
                 stream.Write(data, 0, data.Length);
@@ -38,88 +41,36 @@ namespace LabApp
                 Console.WriteLine("Сообщение отправлено");
             }
 
+
+
             //ApplicationConfiguration.Initialize();
             //Application.Run(new ApplicationForm());
-        
-        } // end of Main
-
-        static void Listening(NetworkStream stream)
-        {
-
-            string receveMsg;
-            int bytesCounter = 0;
-
-            while (true)
-            {
-                int messageSize = 0;
-                char buf = ' ';
-                while (buf != '|')
-                {
-                    buf = (char)stream.ReadByte();
-
-                    if (!Char.IsDigit(buf) || buf == '|')
-                        return;                                             //need to throw Exeption or do smth
-                    else
-                        messageSize += (int)buf;                    
-                }
-
-                byte[] buffer = new byte[messageSize];
-                while(bytesCounter <= messageSize)
-                {
-                    bytesCounter = stream.Read(buffer, 0, messageSize);
-                }
-
-                receveMsg = Encoding.UTF8.GetString(buffer);
-
-                ServerResponseInterpretator(receveMsg);
-
-                //if (readBytes == 0)
-                //{
-                //    Console.WriteLine("Сервер завершил работу");
-                //    break;
-                //}
-
-                //receveMsg = Encoding.UTF8.GetString(data, 0 , readBytes);
-
-                //ServerResponseInterpretator(receveMsg);
-
-
-            }
-
-        } // end of Listener
-
-        static void ServerResponseInterpretator(string response)
-        {
-            if (response.IsWhiteSpace() || string.IsNullOrEmpty(response))
-                return;                                                         //need to throw Exeption or do smth
-
-            string[] parts = response.Split('|');
-
-            if (!int.TryParse(parts[0], out int dataSyze))
-                return;
-
-            if(!Enum.TryParse<ServerResponse>(parts[1], out ServerResponse command))
-            {
-                return;                                                          //need to throw Exeption or do smth
-            }
-            else
-            {
-                switch (command)
-                {
-                    case (ServerResponse.CLIENTS_INFO):
-                        foreach (string s in parts)
-                        {
-                            Console.WriteLine(s);
-                        }
-                        break;
-
-                    default:
-                        break;                                                  //need to throw Exeption or do smth
-                }
-            }
-            
         }
 
-    } // end of class Program
 
-} // end of LabApp
+        static void Listener(NetworkStream stream)
+        {
+            while (true)
+            {
+
+                byte[] data = new byte[1024];
+
+                int readBytes = stream.Read(data, 0, data.Length);
+
+                if (readBytes == 0)
+                {
+                    Console.WriteLine("Сервер завершил работу");
+                    break;
+                }
+
+                string receveMsg = Encoding.UTF8.GetString(data, 0, readBytes);
+
+                Console.WriteLine(receveMsg);
+
+            }
+
+        }
+
+    }
+
+}
---
+++ ProductTcpServer/ClientSession.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ProductTcpServer
+{
+    public class ClientSession
+    {
+        public ClientSession(TcpClient client)
+        {
+            Client = client;
+            Stream = client.GetStream();
+        }
+
+        public string? Name { get; private set; }
+
+        public bool IsLogged => Name != null;
+
+        public TcpClient Client     { get; private set; }
+
+        public NetworkStream Stream { get; private set; }
+
+        public void Login(string username)
+        {
+            if (IsLogged)
+                throw new Exception("Already logged in");
+            Name = username;
+        }
+    }
+}
